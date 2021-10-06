0679b4c35960aa9f8110564b26fcb49851e3e227
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-06 20:44:27 +0000

    formatting functions

--- joystickClass.py
+++ joystickClass.py
@@ -5,10 +5,10 @@ class Joystick:
   def __init__(self, address):
     self.PCF8591 = PCF8591(address)
     #PCF8591(0x40)
-    def getx():
-      #print(PCF8591.read(1))
-      return PCF8591.read(1)
+  def getx():
+    #print(PCF8591.read(1))
+    return PCF8591.read(1)
 
-    def gety():
-      #print(PCF8591.read(2))
-      return PCF8591.read(2)
+  def gety():
+    #print(PCF8591.read(2))
+    return PCF8591.read(2)
