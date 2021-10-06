9e7ed053df03c0c902b83318b880ee57ef4bf5e6
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-06 20:24:50 +0000

    Separate Files for Classes and other Miscellaneous Things

---
+++ joystickClass.py
@@ -0,0 +1,19 @@
+from time import sleep
+from PCF import PCF8591
+
+class Joystick:
+  def __init__(self, address, x, y):
+    self.PCF8591 = PCF8591(address, x, y)
+    PCF8591(0x40)
+    self.x = self.getx()
+    self.y = self.gety()
+    def getx():
+      #print(PCF8591.read(1))
+      return PCF8591.read(1)
+
+    def gety():
+      #print(PCF8591.read(2))
+      return PCF8591.read(2)
+
+    print('%s, %s'.format(self.x, self.y))
+    sleep(100)
--- main.py
+++ main.py
@@ -1,41 +1,16 @@
 #   To check address: sudo i2cdetect -y 1
 
+from joystickClass import joystick
+from PCF import PCF8591
 import smbus
 from time import sleep
 
-class PCF8591:
-
-  def __init__(self,address):
-    self.bus = smbus.SMBus(1)
-    self.address = address
-
-  def read(self,chn): #channel
-      try:
-          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
-          self.bus.read_byte(self.address) # dummy read to start conversion
-      except Exception as e:
-          print ("Address: %s \n%s" % (self.address,e))
-      return self.bus.read_byte(self.address)
-
-  def write(self,val):
-      try:
-          self.bus.write_byte_data(self.address, 0x40, int(val))
-      except Exception as e:
-          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
-
-class Joystick:
-  def __init__(self, address, x, y):
-    self.PCF8591 = PCF8591(address, x, y)
-    PCF8591(address)
-    self.x = self.getx()
-    self.y = self.gety()
-    def getx():
-      #print(PCF8591.read(1))
-      return PCF8591.read(1)
-
-    def gety():
-      #print(PCF8591.read(2))
-      return PCF8591.read(2)
-      sleep(100)
-
-    print('%s, %s'.format(self.x, self.y))
+while True:
+  try:
+    joystick(0x40)
+    x = joystick.getx()
+    y = joystick.gety()
+    print('%s, %s'.format(x,y))
+  
+  except KeyboardInterrupt:
+    print('shutting down joystick')
