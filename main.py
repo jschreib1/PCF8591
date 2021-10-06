07eea0f159d9ec816b6923143af9f09395e3305e
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-06 20:54:50 +0000

    Format

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from PCF import PCF8591
 import smbus
 from time import sleep
 
-print('Working')
 while True:
-  print("Working in the While Loop")
   try:
     myJoystick = Joystick(0x48)
     x = myJoystick.getx()
     y = myJoystick.gety()
-    print('{:d}, {:d}'.format(x,y))
+    print('{:s}, {:s}'.format(str(x).rjust(5),str(y).rjust(5)))
     sleep(0.1)
   
   except KeyboardInterrupt:
